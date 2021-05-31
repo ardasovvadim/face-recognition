1453d00ed1c26db2c1348a8251fc9e13752bd52e
Vadim <32977236+ardasovvadim@users.noreply.github.com>
2021-05-31 14:45:14 +0200

    Delete server directory

--- server/aiohttp_server.py
+++
@@ -1,8 +0,0 @@
-import socketio
-from aiohttp import web
-
-if __name__ == '__main__':
-    sio = socketio.AsyncServer(async_mode='aiohttp')
-    app = web.Application()
-    sio.attach(app)
-    web.run_app(app)
